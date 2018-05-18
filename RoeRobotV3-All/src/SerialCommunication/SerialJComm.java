24e3edfc1db4abf15974d48ddc9f4ff5ddadcd45
pejhej <36162293+pejhej@users.noreply.github.com>
2018-05-18 18:07:01 +0000

    Add files via upload
    new serialJ from pej

--- RoeRobotV3-All/src/SerialCommunication/SerialJComm.java
+++ RoeRobotV3-All/src/SerialCommunication/SerialJComm.java
@@ -14,6 +14,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,7 +22,7 @@ import java.util.logging.Logger;
  *
  * @author Yngve
  */
-public class SerialJComm
+public class SerialJComm extends Thread
 {
 
     //The serial port
@@ -29,12 +30,16 @@ public class SerialJComm
 
     //The serial port name
     private String portName;
+    
+      // variable holding the timeout variable
+    private static final int TIME_OUT = 2000;
+
+    // variable holding the desired rate of sending and receiving data 
+    private static final int DATA_RATE = 115200;
 
     //Reader and writer stream
     InputStream reader;
     OutputStream writer;
-    
-    
 
     //Buffered reader
     // variable holding the bufferedreader
@@ -45,35 +50,29 @@ public class SerialJComm
 
     //Variable holding the data to be sent to the Arduino
     private byte[] dataToBeSent;
-    
+    private boolean dataToSend = false;
+
     private String[] inputStringData;
     private byte[] inputByteData;
-    
-    
+
     public SerialJComm(String portName)
     {
-        
-        
+
         //Print the port name
         getPortNames();
-        
         this.portName = portName;
-        
-           // creating the arrayList of listeners 
+
+        // creating the arrayList of listeners 
         this.listeners = new ArrayList<>();
 
-        this.inputStringData = new String[20];
-
-        
-     
-
+        //this.inputStringData = new String[20];
     }
+
     /**
      * Connect to the serial port, with the portname in the constructor
      */
     public void connect()
     {
-                
         //Find the port
         port = findPort(portName);
         try
@@ -81,14 +80,13 @@ public class SerialJComm
             System.out.println("Opening Port. ");
             Thread.sleep(500);
             initializePort();
-            Thread.sleep(2000);
+            Thread.sleep(1000);
             //Return the streams from the port
             //System.out.println("Setup done");
-            
 
         } catch (InterruptedException ex)
         {
-                        System.out.println("Could not open port. ");
+            System.out.println("Could not open port. ");
             Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
         }
 
@@ -96,36 +94,36 @@ public class SerialJComm
         if (this.port.isOpen())
         {
             System.out.println("Port is open");
-        }
-        else
+        } else
         {
             System.out.println("Port is NOT open");
         }
     }
-    
+
     /**
      * Get the input and output streams from the SerialPort interface
      */
     private void initializePort()
     {
         //Opening the port and setting the buad rate
-       // this.port = SerialPort.getCommPort(portName);
-        this.port.setBaudRate(9600);
+        // this.port = SerialPort.getCommPort(portName);
+        this.port.setBaudRate(DATA_RATE);
+        //this.port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 100, 0);
         //Try to open the port
         this.port.openPort();
+        port.setComPortTimeouts(port.TIMEOUT_READ_SEMI_BLOCKING, 0,0);
         //Return the writer stream
-        writer = port.getOutputStream();
-        
-        
+        //  writer = port.getOutputStream();
+
         //Return the input stream
-        reader = port.getInputStream();
-        
+        //reader = port.getInputStream();
         // creates an inputstream for reading data
         this.input = new BufferedReader(new InputStreamReader(this.port.getInputStream()));
 
-        System.out.println("Settings for port is done");
+        // System.out.println("Settings for port is done");
         //Adding listener and implementing the serial event
         // add eventlisteners   
+        /*
         port.addDataListener(new SerialPortDataListener()
         {
             @Override
@@ -137,54 +135,116 @@ public class SerialJComm
             @Override
             public synchronized void serialEvent(com.fazecast.jSerialComm.SerialPortEvent event)
             {
-               if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
                 {
                     return;
                 }
-         
-                try
+
+                if (port.bytesAvailable() == 0)
                 {
-                    if(reader.available() > 0)
+                    /*
+                    try
                     {
-                        try
-                        {
-                            //A little sleep for to get all data on the line
-                            Thread.sleep(5);
-                        } catch (InterruptedException ex)
-                        {
-                            Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        //ByteArr to store the incomming data in
-                         byte[] newData = new byte[reader.available()];
-                        int numRead = reader.read(newData);
-                        System.out.println("Read " + numRead + " bytes.");
-                            //Print the string
-                        printByteArr(newData);
-                        //Send notify
-                        notifyListeners(newData);
+                        //A little sleep for to get all data on the line
+                        Thread.sleep(5);
+                    } catch (InterruptedException ex)
+                    {
+                        Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                              
-                } catch (IOException ex)
-                {
-                    Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
-                }   
+                     */
+        /*
+                    try
+                    {
+                        String[] inputStringArr = input.readLine().split(",");
+                        notifyListeners(inputStringArr);
+                    } catch (IOException ex)
+                    {
+                        System.out.println("Reader threw exception");
+                        Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                } 
             }
         });
-
-       
-
-        
+        */
+    }
+    
+           @Override
+    public void run()
+    {
+        //While loop to keep thread running as long as port is open
+        while (port.isOpen())
+        {
+            //Checks if data is availble to be sent
+            if (dataToSend)
+            {
+                this.sendData();
+                dataToSend = false;
+            }
+            
+            try
+            {
+                //read from serial port
+                while (input.ready())
+                {
+                    //SLEEP FOR THE ALL THE BYTES INCOMMING ON THE SERIAL TO GET IN THE BUFFER BEFORE READING STARTS
+                    
+                    try
+                    {
+                        Thread.sleep(10);
+                    } catch (InterruptedException ex)
+                    {
+                        System.out.println("SerialJCOMM has interrupted sleep..");
+                        Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    
+                    //Uses INPUT READ BUFFER to read a line until \n is in the line
+                    try
+                    {
+                        String[] dataArrString = input.readLine().split(",");
+                        System.out.println(Arrays.toString(dataArrString));
+                        notifyListeners(dataArrString);
+                        
+                    } catch (UnsupportedEncodingException ex)
+                    {
+                        Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex)
+                    {
+                        System.out.println("SerialJCOMM port " + this.portName + " got IO exception..");
+                        Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            } catch (IOException ex)
+            {
+                Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        this.close();
     }
 
-    
-    
+
+
+    /**
+     * Print the given byte arr
+     */
+    private void printStringArr(String[] stringArr)
+    {
+        String inputString;
+        System.out.println("INPUT ARR");
+        int size = stringArr.length;
+        for (int i = 0; i < size; ++i)
+        {
+            System.out.println(stringArr[i]);
+        }
+    }
+
     /**
      * Print the given byte arr
      */
     private void printByteArr(byte[] byteArr)
     {
-     
-          String inputString;
+        String inputString;
         try
         {
             inputString = new String(byteArr, "UTF-8");
@@ -195,7 +255,6 @@ public class SerialJComm
             System.out.print("Tried creating string from byte arr");
             Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
         }
-                    
     }
 
     /**
@@ -205,6 +264,7 @@ public class SerialJComm
      */
     private String[] getPortNames()
     {
+        System.out.println("Finding ports");
         SerialPort[] ports = SerialPort.getCommPorts();
         String[] result = new String[ports.length];
         for (int i = 0; i < ports.length; i++)
@@ -212,11 +272,11 @@ public class SerialJComm
             result[i] = ports[i].getSystemPortName();
             System.out.println(result[i]);
         }
-        
+
         return result;
     }
-    
-      /**
+
+    /**
      * Print the port names connected to the SerialPort
      *
      * @return Print the port names connected to the SerialPort
@@ -229,13 +289,32 @@ public class SerialJComm
         for (int i = 0; i < ports.length; i++)
         {
             result[i] = ports[i].getSystemPortName();
-            if(findPort.compareTo(ports[i].getSystemPortName()) == 0)
+            if (findPort.compareTo(ports[i].getSystemPortName()) == 0)
             {
                 foundPort = ports[i];
             }
-            System.out.println(result[i]);
         }
-        
+
+        //Check if the port was not found
+        //Check what the string was, and do another search on possible port name
+        if (foundPort == null)
+        {
+            if (findPort.contentEquals("ttyACM0"))
+            {
+                findPort = "ttyACM1";
+
+                for (int i = 0; i < ports.length; i++)
+                {
+                    result[i] = ports[i].getSystemPortName();
+                    if (findPort.compareTo(ports[i].getSystemPortName()) == 0)
+                    {
+                        foundPort = ports[i];
+                    }
+                }
+
+            }
+        }
+
         return foundPort;
     }
 
@@ -251,15 +330,14 @@ public class SerialJComm
         try
         {
 
-            this.setDataToBeSent(bytesToSend);
+            // this.setDataToBeSent(bytesToSend);
             //Print the data to be sent - for debugging
             System.out.println("Writer sending data");
             String inputString = new String(this.getDataToSend(), "UTF-8");
             System.out.println(inputString);
 
             //Send the data
-            this.writer.write(this.getDataToSend());
-            writer.flush();
+            this.port.writeBytes(this.getDataToSend(), this.getDataToSend().length);
 
         } catch (IOException ex)
         {
@@ -267,12 +345,13 @@ public class SerialJComm
             System.out.println("Serial: " + ex.toString());
         }
     }
-    
-        /**
+
+    /**
      * sends the data received from the function call
      */
-    public void sendData(String stringToSend)
+    public synchronized void sendData(String stringToSend)
     {
+
         // try to send the read data
         try
         {
@@ -286,15 +365,63 @@ public class SerialJComm
 
             //Send the data
             this.port.writeBytes(this.getDataToSend(), this.getDataToSend().length);
-           // port.flush();
+            // port.flush();
 
         } catch (IOException ex)
         {
             ex.printStackTrace();
             System.out.println("Serial: " + ex.toString());
         }
+
     }
-    
+
+    /**
+     * sends the data received from the function call
+     */
+    private synchronized void sendData()
+    {
+
+        // try to send the read data
+        try
+        {
+            //Set data to be sent
+            // this.setDataToBeSent(stringToSend.getBytes("UTF-8"));
+            //Print the data to be sent - for debugging
+            //Data to send
+            System.out.println("Writer sending data");
+            String inputString = new String(this.getDataToSend(), "UTF-8");
+            System.out.println(inputString);
+
+            //Send the data
+            this.port.writeBytes(this.getDataToSend(), this.getDataToSend().length);
+            // port.flush();
+
+        } catch (IOException ex)
+        {
+            ex.printStackTrace();
+            System.out.println("Serial: " + ex.toString());
+        }
+
+    }
+
+    public synchronized void setDataToBeSent(String dataString)
+    {
+        // setting the start and stopbytes of the data to be sent
+        // making it easy for the Arduino to reecognize i this.calculator.getCalculatedData()f the message
+        // is at the beginning when it starts to receive.
+
+        byte[] dataToSend = null;
+        try
+        {
+            this.dataToBeSent = dataString.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException ex)
+        {
+            Logger.getLogger(SerialJComm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        this.dataToSend = true;
+
+    }
 
     private synchronized void setDataToBeSent(byte[] dataByte)
     {
@@ -351,7 +478,7 @@ public class SerialJComm
      * Method notifying notify all listeners of data now available for reading
      * listener has to implement the CalculationListener interface
      */
-    private synchronized void notifyListeners(byte[] input)
+    private synchronized void notifyListeners(String[] input)
     {
         if (this.listeners != null)
         {
@@ -361,15 +488,14 @@ public class SerialJComm
             }
         }
     }
-    
-    
-        /** 
+
+    /**
      * Method closing the connection with the serialport.
      */
-    public synchronized void close() 
+    public synchronized void close()
     {
         // check if there is a instance of a serialport
-        if (this.port != null) 
+        if (this.port != null)
         {
             // remove eventlisteners from the serialport
             this.port.removeDataListener();
@@ -377,7 +503,12 @@ public class SerialJComm
             this.port.closePort();
         }
     }
-
+}
+    
+ 
+    
+    
+    
     /*
     /**
      * Handles event on the serial port. Checks if there is data coming in from
@@ -387,7 +518,7 @@ public class SerialJComm
      *
      * @param oEvent
      */
-    /*
+ /*
     @Override
     public synchronized void serialEvent(SerialPortEvent oEvent)
     {
@@ -418,6 +549,5 @@ public class SerialJComm
             }
         }
     }
-*/
+     */
 
-}
